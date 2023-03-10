91986a12e5c92e85ca46678b0b8d6d464d9dccd2
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-03-10 15:30:18 +0000

    Add additional vite resources

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': process.env,
+    global: {},
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
