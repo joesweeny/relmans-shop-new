97b1773c245455c8e8c4f319a78bb3ba6e8418b0
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-04-13 17:27:17 +0100

    Re-add friday deliveries

--- src/components/Checkout/CheckoutDelivery/CheckoutDate/CheckoutDate.jsx
+++ src/components/Checkout/CheckoutDelivery/CheckoutDate/CheckoutDate.jsx
@@ -42,7 +42,7 @@ const CheckoutDate = (props) => {
 
   const dates = (date) => {
     const day = date.getDay();
-    return day !== 0 && day !== 1 && day !== 5;
+    return day !== 0 && day !== 1;
   };
 
   const minDate = new Date(Date.now() + 3600 * 1000 * 24).setHours(11, 0, 0);
